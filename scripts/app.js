f53946266d78298bfc21e31c48318421ff5a49f2
Instructor 05 <dassumit.ece@gmail.com>
2024-03-18 17:02:43 -0600

    Added imges for products and change logo

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('#productsContainer').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 4000,
     });
