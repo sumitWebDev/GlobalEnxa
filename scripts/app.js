0d281263696ef4e47f42ed517ebc50af23ce59a0
Instructor 05 <dassumit.ece@gmail.com>
2024-03-19 17:41:45 -0600

    Feedbacks\

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,29 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 4000,
         lazyLoad: 'ondemand',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
     $(".carousel.lazy-load").on('slide.bs.carousel', function(ev){
         var lazy;
